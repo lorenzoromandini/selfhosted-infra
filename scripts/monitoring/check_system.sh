2c9b49a5c5ad109cb54ee36dd3fc8a150c6b39cc
Ubuntu <ubuntu@local>
2026-03-02 00:14:39 +0100

    Fix CPU monitoring - use -bn2 for accurate readings

--- scripts/monitoring/check_system.sh
+++ scripts/monitoring/check_system.sh
@@ -58,7 +58,7 @@ print(json.dumps(data))
 previous_state=$(load_state)
 current_state_json="{}"
 
-CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+CPU_USAGE=$(top -bn2 -d 1 | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 CPU_INT=${CPU_USAGE%.*}
 check_and_notify "CPU" "$CPU_INT" "90" "%"
 
